3bdc5e155a48d1047b2ec61763ba95cd9f3cfd4b
Alikhan Anuarbekov <anuarali@fel.cvut.cz>
2025-07-06 11:22:38 +0200

    Moving everything 1 level above x2

--- Logos/Logos.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Logos.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Logos, "Logos" );
- 
--- Logos/LogosGameMode.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "LogosGameMode.h"
-
-ALogosGameMode::ALogosGameMode()
-{
-	// stub
-}
